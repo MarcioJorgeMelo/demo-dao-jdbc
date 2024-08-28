afd454a2ffb946d69a7de290cfee16bb26f03f8d
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-08-28 14:51:53 -0300

    SellerDao and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -18,6 +20,8 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
